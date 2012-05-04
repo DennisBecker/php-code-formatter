c7e00c907ccb3fd1592d9111b1f7af7770bdc07a
Dennis Becker <becker@sipgate.de>
2012-05-04 12:23:29 +0200

    add phpunit.xml for unit tests add coverage.xml to run unit tests with code coverage report add build.xml for ant add preloadMakeGoog.php as preload script for Eclipse plugin MakeGood used for continuous testing in Eclipse add Bootstrap file for unit tests add Autoload file created by phpab tool from Arne Blankerts - thx for this great tool

---
+++ preloadMakeGood.php
@@ -0,0 +1,2 @@
+<?php
+exec("phpab -o " . __DIR__ . "/src/PHP/CodeFormatter/Autoload.php " . __DIR__ . "/src/PHP/CodeFormatter");
---
+++ tests/units/Bootstrap.php
@@ -0,0 +1,2 @@
+<?php
+require __DIR__ . '/../../src/PHP/CodeFormatter/Autoload.php';
